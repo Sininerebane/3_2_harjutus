a0c4bee89b546aeb5c81a82c815e78112d0f66f7
Natalya Berezovski <natalya.berezovski@gmail.com>
2021-01-31 14:01:05 +0200

    Repaired commit

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,30 @@
 public class Main {
     public static void main(String[] agrs) {
-        int ammount1 = 100;
-        int percent = 1;
-        int ammount2 = 1100;
-        int ammount;
+        int ammount = 1100;
+        int initial = 100;
+        int limit = 1001;
+        int  allAmmout;
         int bonus;
         int total;
-        int limit = 1001;
         int all;
-        if ( ammount2 > limit) {
-            bonus = 1;
-            ammount = ammount1 + ammount2;
-        total = ((ammount)/100) * bonus -1;
-        all = ammount + total;
-        System.out.println("Если сумма перевода - 1100 рублей");
-        System.out.println("Бонусы:" + total);
-        System.out.println("Деньги на счету:" + all);         }
-        if ( ammount1 < limit) {
+        if ( ammount < limit) {
             bonus = 0;
-            ammount = ammount1 + 100;
-            total = ((ammount)/100) * bonus;
-            all = ammount + total;
+            allAmmout = ammount + initial;
+            total = (( allAmmout)/100) * bonus;
+            all =  allAmmout + total;
             System.out.println("Если сумма перевода - 100 рублей");
+            System.out.println("Бонусы:" + total);
+            System.out.println("Деньги на счету:" + all);
+        } else {
+            bonus =1;
+            allAmmout = ammount + initial;
+            total = ((allAmmout)/100) * bonus -1;
+            all = allAmmout + total;
+            System.out.println("Если сумма перевода - 1100 рублей");
             System.out.println("Бонусы:" + total);
             System.out.println("Деньги на счету:" + all);
         }
     }
-    }
+}
+
 
